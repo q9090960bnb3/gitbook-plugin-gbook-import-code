e5f89e194e3f6ec5305e3de2b78c4c39af3360fc
huo <huo@mail.com>
2024-06-20 07:14:46 +0000

    较稳定版

--- README.md
+++ README.md
@@ -4,14 +4,29 @@ gitbook plugin support @import "{filename}"
 
 for example:
 
+- 普通使用
+
 ```md
 @import "src/1.go"
-```
+``` 
 
-or 
+- 指定文件类型
 
 ```md
 @import "src/1" {go}
+```
+
+or
+
+```md
+@import "src/1" {lang: 'go'}
+```
+
+- 配合 gbook-codetab 使用, 将2段代码合并为一组
+
+```md
+@import "src/1.js" {title: 'demo', group: 'group01'}
+@import "src/2.js" {group: 'group01'}
 ```
 
 ## use in book.json
--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const fs = require("fs");
 const PathUtil = require("path");
 
+function getInfo(str) {
+  if (/^\w$/.test(str)) {
+    return {
+      lang: str
+    }
+  }
+
+  let validStr = str.replace(/([a-zA-Z_$][0-9a-zA-Z_$]*)\s*:/g, '"$1": ');
+  validStr = validStr.replace(/'/g, '"');
+  return JSON.parse("{" + validStr + "}")
+}
+
 function replaceContent(rawPath, textData) {
   // 将文本数据分割成行
   const lines = textData.split("\n");
@@ -9,17 +21,21 @@ function replaceContent(rawPath, textData) {
 
   for (let i = 0; i < lines.length; i++) {
     if (lines[i].includes("@import")) {
-      const filePath = lines[i].match(/@import "([^"]+)"/);
-      if (filePath) {
+      const res = lines[i].match(/@import "([^"]+)"[ \t]*(?:{(.+)})?/)
+      if (res) {
+        const importPath = PathUtil.isAbsolute(res[1])
+        ? res[1]
+        : rawPath + "/" + res[1];
 
-        const importPath = PathUtil.isAbsolute(filePath[1])
-        ? filePath[1]
-        : rawPath + "/" + filePath[1];
-
-        var extName = PathUtil.extname(importPath).slice(1); 
-        const res = lines[i].match(/@import "([^"]+)" {(.+)}/);
-        if (res){
-          extName = res[2]
+        let extName = PathUtil.extname(importPath).slice(1);
+        let otherInfo = ""
+        if (res[2]){
+          // console.log('res2:', res[2])
+          const info = getInfo(res[2])
+          if (info.lang) {
+            extName = info.ext
+          }
+          otherInfo = JSON.stringify(info)
         }
 
         // console.info("path: ", importPath, "extName: ", extName);
@@ -31,7 +47,8 @@ function replaceContent(rawPath, textData) {
             fileContent += "\n"
           }
           // 替换@import行为文件内容
-          lines[i] = "```" + extName + "\n" + fileContent + "```\n";
+          lines[i] = "```" + extName + " " + otherInfo + "\n" + fileContent + "```\n";
+          // console.log('line:', lines[i])
         } catch (err) {
           console.error(`无法读取文件: ${err}`);
         }
@@ -50,7 +67,6 @@ module.exports = {
   hooks: {
     "page:before": async (page) => {
       page.content = replaceContent(page.rawPath, page.content);
-
       return page;
     },
   },
